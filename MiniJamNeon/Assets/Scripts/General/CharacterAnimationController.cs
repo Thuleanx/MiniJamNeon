f43d763dc73cb7523a927a4617a340aea8319a89
thuleanx <thuleanx@tutanota.com>
2020-07-11 23:27:39 -0500

    Updated scene

---
+++ MiniJamNeon/Assets/Scripts/General/CharacterAnimationController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationController : MonoBehaviour
+{
+	public enum AnimState {
+		Idle = 0,
+		Run = 1,
+		Fall = 2,
+		Jump = 3,
+		Attack = 4,
+		Hurt = 5
+	}	
+}
