4e54ab2c0c4650039c4a88242f7c7d6c9e82ff9f
thuleanx <thuleanx@tutanota.com>
2020-07-12 12:42:48 -0500

    Gun point at mouse working right for 2nd and 3rd quadrants

--- MiniJamNeon/Assets/Scripts/Behavior/PointAtMouseDirection.cs
+++ MiniJamNeon/Assets/Scripts/Behavior/PointAtMouseDirection.cs
@@ -13,6 +13,21 @@ public class PointAtMouseDirection : MonoBehaviour
 	void Update() {
 		Vector2 target = mainCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
 
+		if (target.x < 0) {
+			if (transform.localScale.y > 0)
+				Flip();
+		} else {
+			if (transform.localScale.y < 0) 
+				Flip();
+		}
 		transform.rotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * Mathf.Atan2(target.y, target.x));	
+	}
+
+	void Flip() {
+		transform.localScale = new Vector3(
+			transform.localScale.x,
+			-transform.localScale.y,
+			transform.localScale.z
+		);
 	}
 }
