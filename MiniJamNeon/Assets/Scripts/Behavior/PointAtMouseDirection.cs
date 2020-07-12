7d8eaea531713f544c7be19e9f34f71f122d2cf9
thuleanx <thuleanx@tutanota.com>
2020-07-12 11:51:37 -0500

    Gun Sprite pointing to mouse location

---
+++ MiniJamNeon/Assets/Scripts/Behavior/PointAtMouseDirection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAtMouseDirection : MonoBehaviour
+{
+	Camera mainCamera; 
+
+	void Awake() {
+		mainCamera = Camera.main;
+	}
+
+	void Update() {
+		Vector2 target = mainCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+		transform.rotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * Mathf.Atan2(target.y, target.x));	
+	}
+}
